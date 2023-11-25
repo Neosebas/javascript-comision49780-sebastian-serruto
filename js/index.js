a981296ce98782925b41d078d98748242ac54039
sebastian serruto <sebastianserruto@gmail.com>
2023-11-25 17:13:09 -0300

    Agregados para la segunda entrega, no son parte del proyecto final

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*** Condicional  ***/
 
-const NUMERO_SECRETO = 8;
+/** const NUMERO_SECRETO = 8;
 let intentos = 0;
 let tuRespuesta ;
 
@@ -12,20 +12,19 @@ while (tuRespuesta !== NUMERO_SECRETO){
     alert("Segui probando");
     }
 }
-alert("Felicitaciones, sos groso")
+alert("Felicitaciones, sos groso") **/
 
 
 
 /*** Para el Proyecto Final  ***/
 
-let NEW_TASK="";
+let newTask="";
 let continuar;
-const DELETE = prompt("Deseas eliminar esta tarea, contesta si o no").toLowerCase();
 
 function tarea () {
     do {
-    const NEW_TASK = prompt("Ingresa una nueva tarea");
-    NEW_TASK =="";
+    const newTask = prompt("Ingresa una nueva tarea");
+    newTask =="";
     continuar = prompt("quieres agregar una nueva tarea").toLowerCase();
 
 }while (continuar === "si");
@@ -35,8 +34,43 @@ function tarea () {
 
 tarea ()
 
+const DELETE = prompt("Deseas eliminar esta tarea, contesta si o no").toLowerCase();
+
 if (DELETE === "si") {
     alert ("Tu tarea se elimino correctamente");
 } else {
     alert ("Esta tarea todavia esta pendiente");
 }
+
+
+/*** Productos y cards para cumplir con la segunda pre-entrega ***/
+
+class CargarProductos {
+    constructor(img, producto, marca, precio){
+        this.img = img;
+        this.producto = producto,
+        this.marca = marca,
+        this.precio = precio;
+    }
+}
+
+
+const ANOTADOR = new CargarProductos("../assets/img/anotador_husares.webp","Anotardor", "Husares", 1500);
+const LAPICERA = new CargarProductos("../assets/img/lapicera_paper_mate.webp", "Lapicera", "Paper Mate", 950);
+const CORRECTOR = new CargarProductos("../assets/img/corrector_liquid_paper.webp", "Corrector","Liquid Paper", 1200);
+
+const PRODUCTOS = [ANOTADOR, LAPICERA, CORRECTOR];
+
+PRODUCTOS.forEach(CargarProductos => {
+    let div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+                    <img src="${CargarProductos.img}"  alt="...">
+                    <div>
+                        <h5>${CargarProductos.producto}</h5>
+                        <p>${CargarProductos.marca}</p>
+                        <p>${CargarProductos.precio}</p>
+                    </div>
+    `
+    cards.appendChild(div)
+})
